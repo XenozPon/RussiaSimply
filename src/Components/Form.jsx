dafd10475c5f6bc6b279806b0d1393fe1d7310ad
x9noz <ilaparusev@gmail.com>
2025-04-09 08:53:09 +0400

    formfixx1

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -5,7 +5,7 @@ const Form = () => {
     function sendEmail(e) {
         e.preventDefault();    //This is important, i'm not sure why, but the email won't send without it
 
-        emailjs.sendForm('sr_1121', 'template_gc45bm8', e.target, 'P13ou8777pqiJaGjJ')
+        emailjs.sendForm('service_simplerussia', 'template_gc45bm8', e.target, 'P13ou8777pqiJaGjJ')
             .then((result) => {
                 alert("Форма отправлена, сайт перезагружен");
                 window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior)
@@ -29,7 +29,7 @@ const Form = () => {
 
                 <h2>Форма обратной связи</h2>
 
-                <form  onSubmit={sendEmail}>
+                <form  onSubmit={sendEmail} method="POST">
                     <div className="form-group">
                         <label htmlFor="case-title">Название кейса</label>
                         <input type="text" id="case-title" name="case-title" required/>
